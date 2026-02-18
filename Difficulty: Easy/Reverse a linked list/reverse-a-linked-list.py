b023d2bdb1a5e08cf21926a3ee572a4de76d46b3
K.Rakshithasri <97074256+rakshithasri06@users.noreply.github.com>
2026-02-18 21:51:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.py
@@ -0,0 +1,20 @@
+"""
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+"""
+
+class Solution:
+    def reverseList(self, head):
+        
+        prev,curr=None,head
+        while curr:
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+            
+        return prev
+        # Code here
+        
