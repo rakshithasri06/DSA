69f88d3321d8ec83a4cdfba182c670bb63dbbe7c
K.Rakshithasri <97074256+rakshithasri06@users.noreply.github.com>
2026-08-04 10:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (37.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,36 @@
 class Solution(object):
     def searchRange(self, nums, target):
-
-        def firstOccurrence():
-            l, r = 0, len(nums) - 1
-            ans = -1
-
-            while l <= r:
-                mid = (l + r) // 2
-
-                if nums[mid] == target:
-                    ans = mid
-                    r = mid - 1      # Look for an earlier occurrence
-                elif nums[mid] < target:
-                    l = mid + 1
+        def lowerbound(nums,target):
+            l=0
+            ans=len(nums)
+            r=ans-1
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]>= target:
+                    ans=mid
+                    r=mid-1
                 else:
-                    r = mid - 1
-
-            return ans
-
-        def lastOccurrence():
-            l, r = 0, len(nums) - 1
-            ans = -1
-
-            while l <= r:
-                mid = (l + r) // 2
-
-                if nums[mid] == target:
-                    ans = mid
-                    l = mid + 1      # Look for a later occurrence
-                elif nums[mid] < target:
-                    l = mid + 1
+                    l=mid+1
+            return ans 
+        def upperbound(nums,target):
+            l=0
+            ans=len(nums)
+            r=ans-1
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]> target:
+                    ans=mid
+                    r=mid-1
                 else:
-                    r = mid - 1
-
+                    l=mid+1
             return ans
+        first=lowerbound(nums,target)
+        if first == len(nums) or nums[first] != target:
+            return [-1,-1]
+        last=upperbound(nums,target)-1
 
-        return [firstOccurrence(), lastOccurrence()]
+        return [first,last]
+
         """
         :type nums: List[int]
         :type target: int
